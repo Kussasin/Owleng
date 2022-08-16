3ccde1cbc310d6126674c7f0ed815195b9543656
Bohdan <bohdan.basistyi@gmail.com>
2022-08-16 21:53:31 +0200

    edit README

--- client/src/index.js
+++ client/src/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -8,8 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
-
 
 reportWebVitals();
